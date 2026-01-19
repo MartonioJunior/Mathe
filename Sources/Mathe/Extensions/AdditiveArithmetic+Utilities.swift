bb969eb7540adf5b4e6c1512a6e30134cc0acf3c
Martônio Júnior <martoniojunior@hotmail.com>
2026-01-19 14:20:35 -0300

    feat(Mathe): Greatest common denominator

---
+++ Sources/Mathe/Extensions/AdditiveArithmetic+Utilities.swift
@@ -0,0 +1,26 @@
+//
+//  AdditiveArithmetic+Utilities.swift
+//  Mathe
+//
+//  Created by Martônio Júnior on 19/01/2026.
+//
+
+// MARK: Self: Comparable
+public extension AdditiveArithmetic where Self: Comparable {
+    static func gcd(_ lhs: Self, _ rhs: Self) -> Self {
+        if lhs == .zero || rhs == .zero { return .zero }
+
+        var lhs = lhs
+        var rhs = rhs
+
+        while lhs != rhs {
+            if lhs > rhs {
+                lhs -= rhs
+            } else {
+                rhs -= lhs
+            }
+        }
+
+        return lhs
+    }
+}
