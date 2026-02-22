c302a3240ec6dcf8932f7037529369ef9ff905a9
Martônio Júnior <martoniojunior@hotmail.com>
2026-02-22 20:36:59 -0300

    feat(Mathe): Module now reexports all other modules in the package

---
+++ Sources/Mathe/Reexports.swift
@@ -0,0 +1,12 @@
+//
+//  Reexports.swift
+//  Mathe
+//
+//  Created by Martônio Júnior on 22/02/26.
+//
+
+@_exported import MatheCoordinates
+@_exported import MatheGeometry
+@_exported import MatheGraphs
+@_exported import MatheRange
+@_exported import MatheSIMD
