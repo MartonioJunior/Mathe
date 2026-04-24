191a49ac46a47b17bf5e0f6485058451ce3c93f0
Martônio Júnior <martoniojunior@hotmail.com>
2026-04-23 21:53:05 -0300

    chore: Renamed file

--- Sources/Mathe/Extensions/FloatingPoint+Utilities.swift
+++ Sources/Mathe/Extensions/FloatingPoint+Utilities.swift
@@ -1,10 +1,11 @@
 //
-//  FloatingPoint+Division.swift
+//  FloatingPoint+Utilities.swift
 //  Mathe
 //
 //  Created by Martônio Júnior on 10/01/2026.
 //
 
+// MARK: Least Common Multiplier
 public extension FloatingPoint {
     static func lcm(_ lhs: Self, _ rhs: Self) -> Self {
         if lhs == 0 || rhs == 0 { return .zero }
