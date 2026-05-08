9a7f54c615d70ffad0ff75821719d9fcd7699332
Martônio Júnior <martoniojunior@hotmail.com>
2026-05-08 15:21:02 -0300

    change(MatheCoordinates): `CartesianCoordinate`
    - Reworked type to use integer generic parameters, becoming a multidimensional coordinate
    - Added documentation for the type

--- Sources/MatheCoordinates/List/CartesianCoordinate.swift
+++ Sources/MatheCoordinates/List/CartesianCoordinate.swift
@@ -5,18 +5,73 @@
 //  Created by Martônio Júnior on 15/08/2025.
 //
 
-public struct CartesianCoordinate {
+public import MatheSIMD
+
+/// Coordinate that defines position in relation to a set of axises
+/// 
+/// Assumes that the base is axis-aligned
+@available(macOS 26.0, *)
+@dynamicMemberLookup
+public struct CartesianCoordinate<let N: Int, Scalar: AdditiveArithmetic> {
     // MARK: Variables
-    public var x: Value
-    public var y: Value
+    var base: Vector<N, Scalar>
+
+    public subscript<T>(dynamicMember keyPath: KeyPath<Vector<N, Scalar>, T>) -> T {
+        base[keyPath: keyPath]
+    }
+
+    public subscript<T>(dynamicMember keyPath: WritableKeyPath<Vector<N, Scalar>, T>) -> T {
+        get { base[keyPath: keyPath] }
+        set { base[keyPath: keyPath] = newValue }
+    }
+
+    // MARK: Initializers
+    init(base: Vector<N, Scalar>) {
+        self.base = base
+    }
+
+    init(_ values: [N of Scalar]) {
+        self.init(base: .init(values))
+    }
 }
 
 // MARK: Self: CoordinateSystem
-extension CartesianCoordinate: CoordinateSystem {}
+@available(macOS 26.0, *)
+extension CartesianCoordinate: CoordinateSystem {
+    public var components: Vector<N, Scalar> { base }
+}
+
+// MARK: Self: Equatable
+@available(macOS 26.0, *)
+extension CartesianCoordinate: Equatable where Scalar: Equatable {}
+
+// MARK: Self: Pointwise
+@available(macOS 26.0, *)
+extension CartesianCoordinate: Pointwise {
+    public var scalarCount: Int { base.scalarCount }
+
+    public subscript(index: Int) -> Scalar {
+        get { base[index] }
+        set { base[index] = newValue }
+    }
+
+    public init(scalars: [Scalar]) {
+        self.init(base: .init(scalars: scalars))
+    }
+}
 
 // MARK: CoordinateSystem (EX)
-public extension CoordinateSystem where Self == CartesianCoordinate {
-    static func cartesian(x: Value, y: Value) -> Self {
-        .init(x: x, y: y)
+@available(macOS 26.0, *)
+public extension CoordinateSystem {
+    static func cartesian<T>(x: T, y: T) -> Self where Self == CartesianCoordinate<2, T> {
+        .init([x, y])
+    }
+
+    static func cartesian<T>(x: T, y: T, z: T) -> Self where Self == CartesianCoordinate<3, T> {
+        .init([x, y, z])
+    }
+
+    static func cartesian<let N: Int, T>(_ vector: Vector<N, T>) -> Self  where Self == CartesianCoordinate<N, T> {
+        .init(base: vector)
     }
 }
