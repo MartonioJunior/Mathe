008dc211cd7f3d4a5b5932cef15e7c4c0ffbb32a
Martônio Júnior <martoniojunior@hotmail.com>
2026-05-08 15:50:00 -0300

    change(MatheCoordinates): `MalleableCoordinate`
    - Adapted generics to keep conformance to `CoordinateSystem`
    - Added conformance to `Equatable`
    - Added documentation for the type

--- Sources/MatheCoordinates/List/MalleableCoordinate.swift
+++ Sources/MatheCoordinates/List/MalleableCoordinate.swift
@@ -5,23 +5,34 @@
 //  Created by Martônio Júnior on 25/11/2025.
 //
 
-public struct MalleableCoordinate<Value> {
+/// Coordinate system where it's position can be represented by any number of values
+/// 
+/// This allows the coordinate to change dimensions with no issues
+public struct MalleableCoordinate<Scalar: AdditiveArithmetic> {
     // MARK: Variables
-    var values: [Value]
+    public var components: [Scalar]
+
+    // MARK: Initializers
+    public init(_ components: [Scalar]) {
+        self.components = components
+    }
 
     // MARK: Subscripts
-    public subscript(axis: Int) -> Value {
-        get { values[axis] }
-        set { values[axis] = newValue }
+    public subscript(axis: Int) -> Scalar {
+        get { components[axis] }
+        set { components[axis] = newValue }
     }
 }
 
 // MARK: Self: CoordinateSystem
 extension MalleableCoordinate: CoordinateSystem {}
 
+// MARK: Self: Equatable
+extension MalleableCoordinate: Equatable {}
+
 // MARK: Self: ExpressibleByArrayLiteral
 extension MalleableCoordinate: ExpressibleByArrayLiteral {
-    public init(arrayLiteral elements: Value...) {
-        self.init(values: elements)
+    public init(arrayLiteral elements: Scalar...) {
+        self.init(elements)
     }
 }
