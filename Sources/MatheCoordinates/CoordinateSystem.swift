ddd159b30499aef5155e3f951c5082b69b26322f
Martônio Júnior <martoniojunior@hotmail.com>
2026-05-08 15:23:43 -0300

    change(MatheCoordinates): `CoordinateSystem`
    - Replaced `Value` with `Scalar`, which now requires an `AdditiveArithmetic` conformance
    - Added documentation for the type

--- Sources/MatheCoordinates/CoordinateSystem.swift
+++ Sources/MatheCoordinates/CoordinateSystem.swift
@@ -5,14 +5,19 @@
 //  Created by Martônio Júnior on 12/07/2025.
 //
 
+/// Data structure that defines a dimensional positioning coordinate
 public protocol CoordinateSystem {
+    /// Vector containing all components that describe this position
     associatedtype Components = Never
-    typealias Value = Double
+    /// Type representing the numerical value used for one component
+    associatedtype Scalar: AdditiveArithmetic = Double
 
+    /// Collection of ordered numbers that uniquely describe this coordinate position
     var components: Components { get }
 }
 
 // MARK: Self.Value == Never
 public extension CoordinateSystem where Components == Never {
+    // swiftlint:disable:next missing_docs
     var components: Never { fatalError("Components vector was not defined!") }
 }
