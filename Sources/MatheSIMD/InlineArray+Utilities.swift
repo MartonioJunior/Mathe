fb57ba2de02885397e20f2d974cdff115cd11216
Martônio Júnior <martoniojunior@hotmail.com>
2026-05-11 00:47:04 -0300

    change(MatheSIMD): `InlineArray` extension
    - Added support for typed throws
    - Added documentation for the type

--- Sources/MatheSIMD/InlineArray+Utilities.swift
+++ Sources/MatheSIMD/InlineArray+Utilities.swift
@@ -7,15 +7,21 @@
 
 @available(macOS 26.0, *)
 internal extension InlineArray {
+    /// Creates a flexible array of elements.
     var flexible: [Element] { indices.map { self[$0] } }
-
-    func forEach(_ action: (Element) throws -> Void) rethrows {
+    /// Iterates through all elements of an inline array.
+    /// - Parameter action: Closure executed for each element.
+    /// - Throws: Error `E` when an action encounters a problem.
+    func forEach<E: Error>(_ action: (Element) throws(E) -> Void) rethrows {
         for i in indices {
             try action(self[i])
         }
     }
-
-    func map<T>(_ transform: (Element) throws -> T) rethrows -> [count of T] {
+    /// Maps each element of the inline array.
+    /// - Parameter transform: Transformation function.
+    /// - Throws: Error `E` when the transformation encounters a problem.
+    /// - Returns: A new `InlineArray` with the transformed elements.
+    func map<T, E: Error>(_ transform: (Element) throws(E) -> T) rethrows -> [count of T] {
         try .init { try transform(self[$0]) }
     }
 }
@@ -23,6 +29,7 @@ internal extension InlineArray {
 // MARK: Self: Equatable
 @available(macOS 26.0, *)
 extension InlineArray: @retroactive Equatable where Element: Equatable {
+    // swiftlint:disable:next missing_docs
     public static func == (lhs: InlineArray<count, Element>, rhs: InlineArray<count, Element>) -> Bool {
         for i in lhs.indices where lhs[i] != rhs[i] {
             return false
