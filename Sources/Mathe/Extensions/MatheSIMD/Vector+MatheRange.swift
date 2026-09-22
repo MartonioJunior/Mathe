59e5fb764afab06f3a6ef1be22b2763ad1777b47
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-22 18:25:19 -0300

    feat(Mathe): `Vector`
    - Added conditional conformances to "MatheRange" main protocols
     - Added `Quad` and `Box` aliases

---
+++ Sources/Mathe/Extensions/MatheSIMD/Vector+MatheRange.swift
@@ -0,0 +1,44 @@
+//
+//  Vector+Range.swift
+//  Trinkets
+//
+//  Created by Martônio Júnior on 01/12/2025.
+//
+
+public import MatheRange
+public import MatheSIMD
+
+@available(macOS 26.0, *)
+public typealias Quad<B: Boundary> = Vector<2, B>
+@available(macOS 26.0, *)
+public typealias Box<B: Boundary> = Vector<3, B>
+
+// MARK: Self: Boundary
+@available(macOS 26.0, *)
+extension Vector: Boundary where Scalar: Boundary {
+    public typealias Bound = Vector<N, Scalar.Bound>
+
+    public static func ~= (lhs: Self, rhs: Bound) -> Bool {
+        lhs.indices.allSatisfy { lhs[$0] ~= rhs[$0] }
+    }
+}
+
+// MARK: Self: Ceiling
+@available(macOS 26.0, *)
+extension Vector: Ceiling where Scalar: Ceiling {
+    public var upperBound: Vector<N, Scalar.Bound> { map(\.upperBound) }
+}
+
+// MARK: Self: Floor
+@available(macOS 26.0, *)
+extension Vector: Floor where Scalar: Floor {
+    public var lowerBound: Vector<N, Scalar.Bound> { map(\.lowerBound) }
+}
+
+// MARK: Self: Gamut
+@available(macOS 26.0, *)
+extension Vector: Gamut where Scalar: Gamut {
+    public init(from lowerBound: Vector<N, Scalar.Bound>, to upperBound: Vector<N, Scalar.Bound>) {
+        self.init(scalars: zip(lowerBound, upperBound).map { Scalar(from: $0, to: $1) })
+    }
+}
