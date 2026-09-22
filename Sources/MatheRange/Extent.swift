32a5d5bfdb5d3812f5bc4844421c48f4bd0eb0f5
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-22 14:57:21 -0300

    docs(MatheRange): `Extent`
    Updated documentation

--- Sources/MatheRange/Extent.swift
+++ Sources/MatheRange/Extent.swift
@@ -5,15 +5,15 @@
 //  Created by Martônio Júnior on 22/04/2026.
 //
 
-/// Closed interval that is a gamut, but not always a boundary.
+/// Gamut that encircles one or more values, independent of bounds order.
 /// 
 /// Different from `ClosedRange` and `Range`, this type does not require `Bound` to conform to `Comparable` to be used
 /// or that it's upper bound is greater or equal than it's lower bound.
 public struct Extent<Bound> {
     // MARK: Variables
-    // swiftlint:disable:next missing_docs
+    /// Start of the extent.
     public var lowerBound: Bound
-    // swiftlint:disable:next missing_docs
+    /// End of the extent.
     public var upperBound: Bound
     // MARK: Initializers
     /// Creates a new extent based on lower and upper bounds.
