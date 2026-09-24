f73456c24e44a38bc10cd5891c35d0192088b5da
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-23 22:22:39 -0300

    feat(MatheCoordinates): `MalleableCoordinate`
    Updated implementation based on changes to `CoordinateSystem`

--- Sources/MatheCoordinates/List/MalleableCoordinate.swift
+++ Sources/MatheCoordinates/List/MalleableCoordinate.swift
@@ -5,33 +5,56 @@
 //  Created by Martônio Júnior on 25/11/2025.
 //
 
+public import MatheSIMD
 /// Coordinate system where it's position can be represented by any number of values
 /// 
 /// This allows the coordinate to change dimensions with no issues
 public struct MalleableCoordinate<Scalar: AdditiveArithmetic> {
     // MARK: Variables
-    public var components: [Scalar]
+    public var values: [Scalar]
 
     // MARK: Initializers
-    public init(_ components: [Scalar]) {
-        self.components = components
+    public init(_ values: [Scalar]) {
+        self.values = values
     }
 
     // MARK: Subscripts
     public subscript(axis: Int) -> Scalar {
-        get { components[axis] }
-        set { components[axis] = newValue }
+        get { values[axis] }
+        set { values[axis] = newValue }
     }
 }
 
 // MARK: Self: CoordinateSystem
-extension MalleableCoordinate: CoordinateSystem {}
+@available(macOS 26.0, *)
+extension MalleableCoordinate: CoordinateSystem {
+    // swiftlint:disable:next missing_docs
+    public typealias Components = DisplacementFor<Self>
+    // swiftlint:disable:next missing_docs
+    public var components: Components { .init(offset: self) }
+    // swiftlint:disable:next missing_docs
+    public func offset(by displacement: Components) -> MalleableCoordinate<Scalar> {
+        .init(zip(values, displacement.offset.values).map { $0.0 + $0.1 })
+    }
+}
+
+// MARK: Self: Pointwise
+@available(macOS 26.0, *)
+extension MalleableCoordinate: Pointwise {
+    // swiftlint:disable:next missing_docs
+    public var scalarCount: Int { values.count }
+    // swiftlint:disable:next missing_docs
+    public init(scalars: [Scalar]) {
+        self.init(scalars)
+    }
+}
 
 // MARK: Self: Equatable
 extension MalleableCoordinate: Equatable {}
 
 // MARK: Self: ExpressibleByArrayLiteral
 extension MalleableCoordinate: ExpressibleByArrayLiteral {
+    // swiftlint:disable:next missing_docs
     public init(arrayLiteral elements: Scalar...) {
         self.init(elements)
     }
