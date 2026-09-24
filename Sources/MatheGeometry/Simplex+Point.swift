a76105717f0dd6f0680f4f3a921e0cec50bb2d08
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-23 22:48:07 -0300

    feat(MatheGeometry) `Simplex`
    Added 0-simplex (`Point`) implementation

---
+++ Sources/MatheGeometry/Simplex+Point.swift
@@ -0,0 +1,17 @@
+//
+//  Simplex+Point.swift
+//  Mathe
+//
+//  Created by Martônio Júnior on 23/09/2026.
+//
+
+@available(macOS 26.0.0, *)
+public typealias Point<Vertex> = Simplex<0, Vertex>
+
+@available(macOS 26.0.0, *)
+public extension Simplex where n == 0, Vertex: Equatable {
+    // swiftlint:disable:next missing_docs
+    static func ~= (lhs: Self, rhs: Vertex) -> Bool {
+        lhs.end == rhs
+    }
+}
