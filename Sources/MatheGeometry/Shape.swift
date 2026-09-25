16620eeda1f4de4aead2b104cdd03d4615bb0dd2
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-25 15:47:06 -0300

    change(MatheGeometry): `Shape`
    - Removed `distanceFromBorder(for:)` and `comparePoint(_:)`, as it requires a new solution
    - Added `isOutline(_:)` and `outline(t:)` methods as requirements for the protocol

--- Sources/MatheGeometry/Shape.swift
+++ Sources/MatheGeometry/Shape.swift
@@ -5,31 +5,28 @@
 //  Created by Martônio Júnior on 02/12/2025.
 //
 
-public import MatheCoordinates
-public import MatheRange
-
 /// Representation of a geometric figure of any dimension.
 public protocol Shape: Geometric {
+    /// Type representing the interpolation parameter used to retrieve a point in the shape's outline.
+    associatedtype OutlineParameter
     /// Is it a closed shape?
     /// 
     /// A closed shape assumes that the end point connects back to the start.
     var isClosed: Bool { get }
-    /// Function that returns the distance of a point to the shape's border.
+    /// Indicates whether a point is part of the shape's border.
     /// - Parameter point: Point in the coordinate space.
-    /// 
-    /// Returns:
-    /// - A negative value when the coordinate is contained within the shape.
-    /// - Zero when the coordinate is tangent to the shape.
-    /// - A positive value when is not part of the shape.
-    func distanceFromBorder(for point: Coordinate) -> Coordinate.Scalar
+    /// Returns: `true` when the point is part of the outline, `false` otherwise.
+    func isOutline(for point: Coordinate) -> Bool
+    /// Defines a position in the shape's outline based on a parameter `t`.
+    /// - Parameter t: Parameter for the interpolation.
+    /// - Returns: Point guaranteed to be part of the outline.
+    func outline(t: OutlineParameter) -> Coordinate
 }
 
-// MARK: Self.Coordinate.Scalar: Comparable
-public extension Shape where Coordinate.Scalar: AdditiveArithmetic & Comparable {
-    /// Checks whether a point is inside or outside of the shape.
-    /// - Parameter point: Coordinate to compare.
-    /// - Returns: `.inside` when point is part of the shape, `.outside` when it's not.
-    func comparePoint(_ point: Coordinate) -> OverlapScore {
-        distanceFromBorder(for: point) > .zero ? .outside() : .inside
-    }
+// MARK: Self.OutlineParameter == Never
+public extension Shape where OutlineParameter == Never {
+    /// Defines a position in the shape's outline based on a parameter `t`.
+    /// - Parameter t: Parameter for the interpolation.
+    /// - Returns: Point guaranteed to be part of the outline.
+    func outline(t _: Never) -> Coordinate {}
 }
