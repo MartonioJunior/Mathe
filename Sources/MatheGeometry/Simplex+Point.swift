b0ec99b8c41a3fea550607d27e49254b135f6695
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-25 12:53:33 -0300

    feat(MatheGeometry): `Simplex`
    Added `rawValue` property when the simplex is a point (n = 0)

--- Sources/MatheGeometry/Simplex+Point.swift
+++ Sources/MatheGeometry/Simplex+Point.swift
@@ -9,6 +9,12 @@
 public typealias Point<Vertex> = Simplex<0, Vertex>
 
 @available(macOS 26.0.0, *)
+public extension Simplex where n == 0 {
+    /// Value associated with the point.
+    var rawValue: Vertex { end }
+}
+
+@available(macOS 26.0.0, *)
 public extension Simplex where n == 0, Vertex: Equatable {
     // swiftlint:disable:next missing_docs
     static func ~= (lhs: Self, rhs: Vertex) -> Bool {
